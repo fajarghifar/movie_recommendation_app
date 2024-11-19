f9ad7a0e644b0aaaebf66e77ecd3f84ac9f31c2c
Fajar Ghifari <fajarghifari3@gmail.com>
2024-11-19 09:50:49 +0700

    feat: add rating and year selection screen

---
+++ lib/features/movie_flow/years_back/years_back.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:movie_recommendation_app/core/constant.dart';
+import 'package:movie_recommendation_app/core/widgets/primary_button.dart';
+
+class YearsBackScreen extends StatefulWidget {
+  const YearsBackScreen({
+    super.key,
+    required this.nextPage,
+    required this.previousPage,
+  });
+
+  final VoidCallback nextPage;
+  final VoidCallback previousPage;
+
+  @override
+  State<YearsBackScreen> createState() => _YearsBackScreenState();
+}
+
+class _YearsBackScreenState extends State<YearsBackScreen> {
+  double yearsBack = 10;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: BackButton(
+          onPressed: widget.previousPage,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              'How many years back should we check for?',
+              style: theme.textTheme.headlineSmall,
+              textAlign: TextAlign.center,
+            ),
+            const Spacer(),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '${yearsBack.ceil()}',
+                  style: theme.textTheme.displayMedium,
+                ),
+                Text(
+                  'Years back',
+                  style: theme.textTheme.headlineMedium?.copyWith(
+                    color: theme.textTheme.headlineMedium?.color
+                        ?.withOpacity(0.62),
+                  ),
+                ),
+              ],
+            ),
+            const Spacer(),
+            Slider(
+              onChanged: (value) {
+                setState(() {
+                  yearsBack = value;
+                });
+              },
+              value: yearsBack,
+              min: 0,
+              max: 70,
+              divisions: 70,
+              label: '${yearsBack.ceil()}',
+            ),
+            const Spacer(),
+            PrimaryButton(
+              onPressed: () {},
+              text: 'Amazing',
+            ),
+            const SizedBox(height: kMediumSpacing),
+          ],
+        ),
+      ),
+    );
+  }
+}
