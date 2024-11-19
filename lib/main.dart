75513f6b9363a850405deb141737851e9778107b
Fajar Ghifari <fajarghifari3@gmail.com>
2024-11-19 22:44:14 +0700

    feat: add controller and provider integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_recommendation_app/features/movie_flow/movie_flow.dart';
 import 'package:movie_recommendation_app/theme/custom_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
