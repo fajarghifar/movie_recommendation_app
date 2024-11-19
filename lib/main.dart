a59147c56b3902b10dc91acd3a39fa161ab14c68
Fajar Ghifari <fajarghifari3@gmail.com>
2024-11-19 09:25:54 +0700

    feat: implement flow of pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_recommendation_app/features/movie_flow/movie_flow.dart';
 import 'package:movie_recommendation_app/theme/custom_theme.dart';
 
 void main() {
@@ -14,21 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Movie Recommendation',
       darkTheme: CustomTheme.darkTheme(context),
       themeMode: ThemeMode.dark,
-      home: const HomeScreen(),
+      home: const MovieFlow(),
     );
-  }
-}
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold();
   }
 }
--- lib/theme/custom_theme.dart
+++ lib/theme/custom_theme.dart
@@ -4,9 +4,25 @@ import 'package:movie_recommendation_app/theme/palette.dart';
 class CustomTheme {
   static ThemeData darkTheme(BuildContext context) {
     final theme = Theme.of(context);
-
     return ThemeData(
-      brightness: Brightness.dark,
+      colorScheme: ColorScheme.fromSwatch(
+        brightness: Brightness.dark,
+        primarySwatch: MaterialColor(
+          Palette.red500.value,
+          const {
+            100: Palette.red100,
+            200: Palette.red200,
+            300: Palette.red300,
+            400: Palette.red400,
+            500: Palette.red500,
+            600: Palette.red600,
+            700: Palette.red700,
+            800: Palette.red800,
+            900: Palette.red900,
+          },
+        ),
+        accentColor: Palette.red500,
+      ),
       scaffoldBackgroundColor: Palette.almostBlack,
       appBarTheme: const AppBarTheme(
         elevation: 0,
@@ -28,21 +44,6 @@ class CustomTheme {
           backgroundColor: Palette.red500,
         ),
       ),
-      colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: MaterialColor(
-        Palette.red500.value,
-        const {
-          100: Palette.red100,
-          200: Palette.red200,
-          300: Palette.red300,
-          400: Palette.red400,
-          500: Palette.red500,
-          600: Palette.red600,
-          700: Palette.red700,
-          800: Palette.red800,
-          900: Palette.red900,
-        },
-      )).copyWith(secondary: Palette.red500),
     );
   }
 }
