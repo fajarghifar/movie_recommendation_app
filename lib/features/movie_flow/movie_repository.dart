5309a1c11f745a96a5226bf0ee66fbcf56b2bbe5
Fajar Ghifari <fajarghifari3@gmail.com>
2024-11-20 08:25:22 +0700

    feat: implement custom failure handling

--- lib/features/movie_flow/movie_repository.dart
+++ lib/features/movie_flow/movie_repository.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_recommendation_app/core/failure.dart';
 import 'package:movie_recommendation_app/env/env.dart';
 import 'package:movie_recommendation_app/features/movie_flow/genre/genre_entity.dart';
 import 'package:movie_recommendation_app/features/movie_flow/result/movie_entity.dart';
@@ -21,41 +24,71 @@ abstract class MovieRepository {
 
 class TMDBMovieRepository implements MovieRepository {
   TMDBMovieRepository({required this.dio});
+
   final Dio dio;
+
   @override
   Future<List<GenreEntity>> getMovieGenres() async {
-    final response = await dio.get(
-      'genre/movie/list',
-      queryParameters: {
-        'api_key': Env.tmdbKey,
-        'language': 'en-US',
-      },
-    );
-    final results = List<Map<String, dynamic>>.from(response.data['genres']);
-    final genres = results.map((e) => GenreEntity.fromMap(e)).toList();
-    return genres;
+    try {
+      final response = await dio.get(
+        'genre/movie/list',
+        queryParameters: {
+          'api_key': Env.tmdbKey,
+          'language': 'en-US',
+        },
+      );
+
+      final results = List<Map<String, dynamic>>.from(response.data['genres']);
+      final genres = results.map((e) => GenreEntity.fromMap(e)).toList();
+
+      return genres;
+    } on DioException catch (e) {
+      if (e.error is SocketException) {
+        throw Failure(
+          message: 'No internet connection',
+          exception: e,
+        );
+      }
+      throw Failure(
+        message: e.response?.statusMessage ?? 'Something went wrong',
+        code: e.response?.statusCode,
+      );
+    }
   }
 
   @override
   Future<List<MovieEntity>> getRecommendedMovies(
       double rating, String date, String genreIds) async {
-    final response = await dio.get(
-      'discover/movie',
-      queryParameters: {
-        'api_key': Env.tmdbKey,
-        'language': 'en-US',
-        'sort_by': 'popularity.desc',
-        'include_adult': false,
-        'vote_average.gte': rating,
-        'page': 1,
-        'release_date.gte': date,
-        'with_genres': genreIds,
-      },
-    );
+    try {
+      final response = await dio.get(
+        'discover/movie',
+        queryParameters: {
+          'api_key': Env.tmdbKey,
+          'language': 'en-US',
+          'sort_by': 'popularity.desc',
+          'include_adult': false,
+          'vote_average.gte': rating,
+          'page': 1,
+          'release_date.gte': date,
+          'with_genres': genreIds,
+        },
+      );
 
-    final results = List<Map<String, dynamic>>.from(response.data['results']);
-    final movies = results.map((e) => MovieEntity.fromMap(e)).toList();
+      final results = List<Map<String, dynamic>>.from(response.data['results']);
+      final movies = results.map((e) => MovieEntity.fromMap(e)).toList();
 
-    return movies;
+      return movies;
+    } on DioException catch (e) {
+      if (e.error is SocketException) {
+        throw Failure(
+          message: 'No internet connection',
+          exception: e,
+        );
+      }
+      throw Failure(
+        message: e.response?.statusMessage ?? 'Something went wrong',
+        code: e.response?.statusCode,
+      );
+    }
   }
 }
