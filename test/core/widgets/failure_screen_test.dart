856ce6676213d4ff32fa2d3a7cd7583ba459f1e7
Fajar Ghifari <fajarghifari3@gmail.com>
2024-11-20 09:27:24 +0700

    test: add widget tests for UI components

---
+++ test/core/widgets/failure_screen_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movie_recommendation_app/core/widgets/failure_screen.dart';
+
+void main() {
+  testWidgets(
+      'Given message When rendering failure screen Then finds that message',
+      (tester) async {
+    const message = 'no';
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: FailureScreen(message: message),
+      ),
+    );
+    expect(find.text(message), findsOneWidget);
+  });
+}
