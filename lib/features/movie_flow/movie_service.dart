79663109609e266390a53c2cd9a4d5ec42c9e150
Fajar Ghifari <fajarghifari3@gmail.com>
2024-11-20 08:42:41 +0700

    feat: support multiple return types in methods

--- lib/features/movie_flow/movie_service.dart
+++ lib/features/movie_flow/movie_service.dart
@@ -1,18 +1,19 @@
 import 'dart:math';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_recommendation_app/core/failure.dart';
 import 'package:movie_recommendation_app/features/movie_flow/genre/genre.dart';
 import 'package:movie_recommendation_app/features/movie_flow/movie_repository.dart';
 import 'package:movie_recommendation_app/features/movie_flow/result/movie.dart';
+import 'package:multiple_result/multiple_result.dart';
 
 final movieServiceProvider = Provider<MovieService>((ref) {
   final movieRepository = ref.watch(movieRepositoryProvider);
-
   return TMDBMovieService(movieRepository);
 });
 
 abstract class MovieService {
-  Future<List<Genre>> getGenres();
-  Future<Movie> getRecommendedMovie(
+  Future<Result<Failure, List<Genre>>> getGenres();
+  Future<Result<Failure, Movie>> getRecommendedMovie(
       int rating, int yearsBack, List<Genre> genres,
       [DateTime? yearsBackFromDate]);
 }
@@ -22,25 +23,38 @@ class TMDBMovieService implements MovieService {
   final MovieRepository _movieRepository;
 
   @override
-  Future<List<Genre>> getGenres() async {
-    final genreEntities = await _movieRepository.getMovieGenres();
-    final genres = genreEntities.map((e) => Genre.fromEntity(e)).toList();
-    return genres;
+  Future<Result<Failure, List<Genre>>> getGenres() async {
+    try {
+      final genreEntities = await _movieRepository.getMovieGenres();
+      final genres = genreEntities.map((e) => Genre.fromEntity(e)).toList();
+      return Success(genres);
+    } on Failure catch (failure) {
+      return Error(failure);
+    }
   }
 
   @override
-  Future<Movie> getRecommendedMovie(
+  Future<Result<Failure, Movie>> getRecommendedMovie(
       int rating, int yearsBack, List<Genre> genres,
       [DateTime? yearsBackFromDate]) async {
     final date = yearsBackFromDate ?? DateTime.now();
     final year = date.year - yearsBack;
     final genreIds = genres.map((e) => e.id).toList().join(',');
-    final movieEntities = await _movieRepository.getRecommendedMovies(
-        rating.toDouble(), '$year-01-01', genreIds);
-    final movies =
-        movieEntities.map((e) => Movie.fromEntity(e, genres)).toList();
-    final rnd = Random();
-    final randomMovie = movies[rnd.nextInt(movies.length)];
-    return randomMovie;
+    try {
+      final movieEntities = await _movieRepository.getRecommendedMovies(
+          rating.toDouble(), '$year-01-01', genreIds);
+      final movies =
+          movieEntities.map((e) => Movie.fromEntity(e, genres)).toList();
+
+      if (movies.isEmpty) {
+        return Error(Failure(message: 'No movies found'));
+      }
+
+      final rnd = Random();
+      final randomMovie = movies[rnd.nextInt(movies.length)];
+      return Success(randomMovie);
+    } on Failure catch (failure) {
+      return Error(failure);
+    }
   }
 }
